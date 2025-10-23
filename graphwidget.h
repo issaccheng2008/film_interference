810c00acc04e826444191727e939c5f6fdab17eb
IC2008 <165460461+issaccheng2008@users.noreply.github.com>
2025-10-23 12:04:55 +0800

    Add interactive ring visualization with sliders

---
+++ graphwidget.h
@@ -0,0 +1,30 @@
+#ifndef GRAPHWIDGET_H
+#define GRAPHWIDGET_H
+
+#include <QWidget>
+#include <QVector>
+
+class GraphWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GraphWidget(QWidget *parent = nullptr);
+
+    void setParameters(double e, double n1, double n2, double n3, double lambda);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    void regenerateData();
+
+    double m_e = 0.0;
+    double m_n1 = 0.0;
+    double m_n2 = 0.0;
+    double m_n3 = 0.0;
+    double m_lambda = 0.0;
+
+    QVector<QPointF> m_samples;
+};
+
+#endif // GRAPHWIDGET_H
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMap>
+#include <QString>
+
+class QSlider;
+class QLabel;
+
+class GraphWidget;
+class RingWidget;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,6 +26,20 @@ public:
     ~MainWindow();
 
 private:
+    void setupUiElements();
+    void updateVisuals();
+
     Ui::MainWindow *ui;
+    RingWidget *m_ringWidget = nullptr;
+    GraphWidget *m_graphWidget = nullptr;
+
+    struct SliderInfo {
+        QSlider *slider = nullptr;
+        QLabel *valueLabel = nullptr;
+    };
+
+    QMap<QString, SliderInfo> m_sliders;
+    QMap<QString, double> m_values;
 };
+
 #endif // MAINWINDOW_H
